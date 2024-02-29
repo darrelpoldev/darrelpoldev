649ea5514e5578a9c873d7306870cf9f9dbaf515
jadewick <darrel.gaming.hk@gmail.com>
2024-02-28 21:07:46 -0600

    creating TodoTable component

--- apps/react-playground-app/src/app/components/ultimate-todo/ultimate-todo.tsx
+++ apps/react-playground-app/src/app/components/ultimate-todo/ultimate-todo.tsx
@@ -21,6 +21,60 @@ const todoInitialState: Todo[] = [
 //  TODO: Create a simple hook as a helper?
 const randomId = () => Math.floor(Math.random() * 9999);
 
+export interface TodoListProps {
+  todoList: Todo[];
+  handleRemove: (todoId: number) => void;
+  handleEdit: (todoId: number) => void;
+  itemIdToEdit: number;
+}
+const TodoTable = (props: TodoListProps) => {
+  const { todoList, handleEdit, handleRemove, itemIdToEdit } = props;
+  return (
+    <div>
+      <h1>Todo List:</h1>
+      {todoList.length > 0 && (
+        <table style={{ borderBlockStyle: 'solid' }}>
+          <thead>
+            <tr>
+              <th>Select</th>
+              <th>Title</th>
+              <th>Description</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {todoList.map((todo) => {
+              return (
+                <tr key={todo.id}>
+                  <td>
+                    <input
+                      onClick={() => handleRemove(todo.id)}
+                      type="checkbox"
+                    />
+                  </td>
+                  <td style={{ padding: '10px' }}>
+                    <b>{todo.title}</b>
+                  </td>
+                  <td>{todo.description}</td>
+                  <td>
+                    <button
+                      hidden={itemIdToEdit != 0}
+                      onClick={() => handleEdit(todo.id)}
+                    >
+                      Edit
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
+      {todoList.length == 0 && <h3>All done for today!</h3>}
+    </div>
+  );
+};
+
 export function UltimateTodo(props: UltimateTodoProps) {
   //  TODO: Convert this to a reducer and add `dateAdded` column
   const [todoList, setTodoList] = useState(todoInitialState);
@@ -118,51 +172,12 @@ export function UltimateTodo(props: UltimateTodoProps) {
         </form>
       </div>
       <hr></hr>
-      <div>
-        {
-          // TODO: Create a new component for the list of TODOs
-        }
-        <h1>Todo List:</h1>
-        {todoList.length > 0 && (
-          <table style={{ borderBlockStyle: 'solid' }}>
-            <thead>
-              <tr>
-                <th>Select</th>
-                <th>Title</th>
-                <th>Description</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              {todoList.map((todo) => {
-                return (
-                  <tr key={todo.id}>
-                    <td>
-                      <input
-                        onClick={() => handleRemove(todo.id)}
-                        type="checkbox"
-                      />
-                    </td>
-                    <td style={{ padding: '10px' }}>
-                      <b>{todo.title}</b>
-                    </td>
-                    <td>{todo.description}</td>
-                    <td>
-                      <button
-                        hidden={itemIdToEdit != 0}
-                        onClick={() => handleEdit(todo.id)}
-                      >
-                        Edit
-                      </button>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        )}
-        {todoList.length == 0 && <h3>All done for today!</h3>}
-      </div>
+      <TodoTable
+        todoList={todoList}
+        handleEdit={handleEdit}
+        handleRemove={handleRemove}
+        itemIdToEdit={itemIdToEdit}
+      />
     </div>
   );
 }
