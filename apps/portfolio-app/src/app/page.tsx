1c852346a853586549f140d0a06b3f1472021380
Darrel Pol <poldarrel.dev@gmail.com>
2024-03-10 21:40:34 -0500

    adding TODO for next sections

--- apps/portfolio-app/src/app/page.tsx
+++ apps/portfolio-app/src/app/page.tsx
@@ -3,6 +3,13 @@ import { Link } from '@chakra-ui/next-js';
 
 export default async function Page() {
   return (
+    //  TODO:
+    //  Create the following pages:
+    //  1. Header
+    //  2. Landing Section
+    //  3. ContactMe Section
+    //  4. Footer
+
     <h1>
       Hello, Next.js!
       <Link href="/about" color="blue.400" _hover={{ color: 'blue.500' }}>
