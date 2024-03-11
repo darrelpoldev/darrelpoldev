29e85125239488f4ed329cea5dd58d88382fdcd9
Darrel Pol <poldarrel.dev@gmail.com>
2024-03-10 21:35:15 -0500

    adding chakraui library with sample implementation

--- apps/portfolio-app/src/app/page.tsx
+++ apps/portfolio-app/src/app/page.tsx
@@ -1,5 +1,13 @@
-import styles from './page.module.css';
+'use client';
+import { Link } from '@chakra-ui/next-js';
 
 export default async function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <h1>
+      Hello, Next.js!
+      <Link href="/about" color="blue.400" _hover={{ color: 'blue.500' }}>
+        About
+      </Link>
+    </h1>
+  );
 }
